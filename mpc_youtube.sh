c4c03d1321736cd50733c1de66fd9d89e6cda606
Rob Pilling <rob pilling at gmail>
2011-07-23 00:35:49 +0100

    Added -n

--- mpc_youtube.sh
+++ mpc_youtube.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+if [ "$1" = '-n' ]
+then
+	BROWSER=echo
+	shift
+fi
+
 [      $# -ne 0 ] && echo "$0: ignoring args" >&2
 [ -z "$BROWSER" ] && BROWSER=firefox
 
 mpc=`mpc | head -1`
-[ -z "$mpc" ] && { echo "$0: no mpc" >&2; exit 1; }
+[ -z "$mpc" ] && { echo "$0: no mpc output" >&2; exit 1; }
 
 "$BROWSER" 'http://duckduckgo.com/?q=!yt '"$mpc"
